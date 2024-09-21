9fb1e341594fd8671b04b217e2ce261993b3c583
Raghu K <raghuk.vit@gmail.com>
2024-09-21 07:30:10 +0530

    RoboShop Kubernetes

--- create-jobs.sh
+++ create-jobs.sh
@@ -2,4 +2,4 @@ if [ -z "${vault_token}" ]; then
   echo vault_token is missing
   exit 1
 fi
-git pull ; ansible-playbook -i localhost, create-jobs.yml -e vault_token=${vault_token}
+git pull ; ansible-playbook create-jobs.yml -e vault_token=${vault_token}
