385619005ea54a00a0eafca2e8b04b27bceda26c
Raghu K <raghuk.vit@gmail.com>
2024-09-23 07:15:22 +0530

    RoboShop Kubernetes

---
+++ vars/ci-pipeline.groovy
@@ -0,0 +1,33 @@
+def call() {
+  node() {
+
+    sh 'env'
+
+    if (env.TAG_NAME) {
+      stage('Docker Build') {
+        print 'Docker Build'
+      }
+
+      stage('Docker Push') {
+        print 'Docker Push'
+      }
+
+      stage('Deploy to Dev') {
+        print 'Deploy to Dev'
+      }
+    }
+    else {
+      stage('Compile') {
+        print 'Compile'
+      }
+
+      if(BRANCH_NAME != "main") {
+        stage('Test Cases') {
+          print 'Test Cases'
+        }
+      }
+
+    }
+
+  }
+}
