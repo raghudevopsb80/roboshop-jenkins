7a61a89077387a34994c07017b41bfa9c49a689e
SVR Kalluru <raghuk.vit@gmail.com>
2024-09-25 06:45:10 +0530

    Ec2 Module

--- vars/ci_pipeline.groovy
+++ vars/ci_pipeline.groovy
@@ -35,7 +35,7 @@ def call() {
 
       stage('Docker Push') {
         sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 633788536644.dkr.ecr.us-east-1.amazonaws.com'
-        sh 'docker build -t 633788536644.dkr.ecr.us-east-1.amazonaws.com/roboshop-cart:${TAG_NAME} .'
+        sh 'docker push 633788536644.dkr.ecr.us-east-1.amazonaws.com/roboshop-cart:${TAG_NAME}'
       }
 
       stage('Deploy to Dev') {
