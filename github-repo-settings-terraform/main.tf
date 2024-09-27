23dc1945ec005acf42c4af2796e1e94ac3fe6aa7
SVR Kalluru <raghuk.vit@gmail.com>
2024-09-27 07:20:11 +0530

    Roboshop Code

--- github-repo-settings-terraform/main.tf
+++ github-repo-settings-terraform/main.tf
@@ -1,15 +1,30 @@
-resource "github_organization_webhook" "main" {
-   name = "raghudevopsb80/roboshop-cart"
+# resource "github_organization_webhook" "main" {
+#    name = "raghudevopsb80/roboshop-cart"
+#
+#   configuration {
+#     url          = "http://jenkins.rdevopsb80.online:8080/multibranch-webhook-trigger/invoke?token=roboshop-cart"
+#     content_type = "form"
+#   }
+#   active = true
+#   events = ["push"]
+# }
+#
+#
+# # provider "github" {
+# #   organization = "raghudevopsb80"
+# # }
+
+
+resource "github_organization_webhook" "foo" {
+  name = "web"
 
   configuration {
-    url          = "http://jenkins.rdevopsb80.online:8080/multibranch-webhook-trigger/invoke?token=roboshop-cart"
+    url          = "https://google.de/"
     content_type = "form"
+    insecure_ssl = false
   }
-  active = true
-  events = ["push"]
+
+  active = false
+
+  events = ["issues"]
 }
-
-
-# provider "github" {
-#   organization = "raghudevopsb80"
-# }
