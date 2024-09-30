390101b11f4c7da292f5219ed7a56936021c7c21
Raghu K <raghuk.vit@gmail.com>
2024-09-30 06:46:34 +0530

    RoboShop Kubernetes

--- github-repo-settings-terraform/main.tf
+++ github-repo-settings-terraform/main.tf
@@ -2,7 +2,7 @@ resource "github_repository_webhook" "main" {
     repository = "roboshop-cart"
 
   configuration {
-    url          = "http://jenkins.rdevopsb80.online:8080/multibranch-webhook-trigger/invoke?token=roboshop-test"
+    url          = "http://jenkins.rdevopsb80.online:8080/multibranch-webhook-trigger/invoke?token=roboshop-cart"
     content_type = "form"
   }
   active = true
