2e5cb55896b99fbad94595c0c821b213daebe867
Raghu K <raghuk.vit@gmail.com>
2024-10-01 06:29:02 +0530

    RoboShop Kubernetes

--- github-repo-settings-terraform/ADD-ENV-GITHUB/main.tf
+++ github-repo-settings-terraform/ADD-ENV-GITHUB/main.tf
@@ -1,10 +1,14 @@
+data "github_user" "user" {
+  username = "r-devops"
+}
+
 resource "github_repository_environment" "env" {
   count               = length(var.ENV)
   environment         = element(var.ENV, count.index)
   repository          = var.repository
   prevent_self_review = false
   reviewers {
-    users = ["r-devops"]
+    users = [data.github_user.user.id]
   }
 }
 
