f28a50813d646f4c58d305ebe9a93d9f402e5fcb
SVR Kalluru <raghuk.vit@gmail.com>
2024-10-10 06:26:14 +0530

    Roboshop Code

--- github-repo-settings-terraform/variables.tf
+++ github-repo-settings-terraform/variables.tf
@@ -1,24 +1,24 @@
 variable "repos" {
   default = {
-    "roboshop-cart" = {}
+    "roboshop-cart"      = {}
     "roboshop-catalogue" = {}
-    "roboshop-user" = {}
-    "roboshop-shipping" = {}
-    "roboshop-payment" = {}
-    "roboshop-frontend" = {}
+    "roboshop-user"      = {}
+    "roboshop-shipping"  = {}
+    "roboshop-payment"   = {}
+    "roboshop-frontend"  = {}
   }
 }
 
 variable "env" {
   default = {
-    "DEV" = {}
-    "QA" = {}
-    "UAT" = {}
+    "DEV"  = {}
+    "QA"   = {}
+    "UAT"  = {}
     "PROD" = {}
   }
 }
 
 locals {
-  repos_with_envs = {for i,j in var.repos : i => { for x,y in var.env : "${i}_${x}" => {"env" = x, "app" = i} }}
-  repo_with_env_list = flatten([for a,b in local.repos_with_envs: values(b)])
+  repos_with_envs    = { for i, j in var.repos : i => { for x, y in var.env : "${i}_${x}" => { "env" = x, "app" = i } } }
+  repo_with_env_list = flatten([for a, b in local.repos_with_envs : values(b)])
 }
